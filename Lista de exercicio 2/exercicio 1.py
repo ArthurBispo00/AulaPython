21a57e20d5d1c4432d8e45e61498f045f19cef39
ArthurBispo00 <arthurbispolima@hotmail.com>
2024-03-19 22:10:02 -0300

    Primeira parte da segunda lista de exercicios, atributos de decisão

---
+++ Lista de exercicio 2/exercicio 1.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um numero: "))
+
+if numero > 10:
+    print("Este numero é maior que 10: ", numero)
+else:
+    print("Este numero não é maior que 10: ", numero )
---
+++ Lista de exercicio 2/exercicio 2.py
@@ -0,0 +1,9 @@
+valor1 = int(input("Digite o primeiro valor: "))
+valor2= int(input("Digite o segundo valor: " ))
+
+if(valor1 > valor2):
+    print("´Primeiro valor é maior que o segundo")
+elif(valor1 == valor2):
+    print("os valores são iguais")
+else:
+    print("O Segundo valor é maior que o primeiro")
